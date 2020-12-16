5d84f00cc483b2a33f72366a5a0718e2f1d268fd
zhouxiukui <871963769@qq.com>
2020-12-16 17:18:23 +0800

    ++

---
+++ min-app/pages/home/home.js
@@ -0,0 +1,56 @@
+const order = ['demo1', 'demo2', 'demo3']
+
+Page({
+  onShareAppMessage() {
+    return {
+      title: 'scroll-view',
+      path: 'page/component/pages/scroll-view/scroll-view'
+    }
+  },
+
+  data: {
+    toView: 'green',
+    imageData:[
+      {src:'../../image/1.jpg',index:1},
+      {src:'../../image/2.jpg',index:2},
+      {src:'../../image/3.jpg',index:3},
+      {src:'../../image/4.jpg',index:4},
+    ]
+  },
+
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  tap() {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+
+  tapMove() {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
+})
